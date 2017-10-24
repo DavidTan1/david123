73082958435dcecec59a25ce8bfe90a3112aaab1
BT_1N3_17 <BT_1N3_17@BT_1N3_17.bktech.local>
2017-10-24 15:34:05 -0400

    go to room

--- src/caveExplorer/CaveRoom.java
+++ src/caveExplorer/CaveRoom.java
@@ -10,13 +10,11 @@ public class CaveRoom {
 	private Door[] doors;
 	private String defaultContents;
 	
-	
 	//constants
 	public static final int NORTH = 0;
 	public static final int EAST = 1;
 	public static final int SOUTH = 2;
 	public static final int WEST = 3;
-	
 	
 	public CaveRoom(String description)
 	{
@@ -26,7 +24,6 @@ public class CaveRoom {
 		borderingRooms = new CaveRoom[4];
 		doors = new Door[4];
 		setDirections();
-		
 	}
 	
 	/**
@@ -35,6 +32,85 @@ public class CaveRoom {
 	 * 
 	 * hint: to check if a door is null use doors[0]==null OR USE doors[0] != null
 	 */
+	
+	public void enter()
+	{
+		contents = "X";
+	}
+	
+	public void leave()
+	{
+		contents = defaultContents;
+	}
+	
+	/**
+	 * This is how we join rooms together
+	 * It gives this room access to anotherRoom and vice-versa
+	 * It also puts the door between both room
+	 * @param direction
+	 * @param anotherRoom
+	 * @param door
+	 */
+	public void setConnection(int direction, CaveRoom anotherRoom, Door door)
+	{
+		addRoom(direction, anotherRoom, door);
+		anotherRoom.addRoom(oppositeDirection(direction),this,door);
+	}
+	public void addRoom(int dir, CaveRoom caveRoom, Door door) 
+	{
+		borderingRooms[dir] = caveRoom;
+		doors[dir] = door;
+		setDirections();
+		
+	}
+	public void interpretInput(String input)
+	{
+		while(!isValid(input))
+		{
+			System.out.println("You can only enter 'w', 'a', 's', or 'd'");
+			input = CaveExplorer.in.nextLine();
+		}
+		int direction = "wdsa".indexOf(input);
+		goToRoom(direction);
+	}
+	private boolean isValid(String input)
+	{
+		return "wasd".indexOf(input) > -1 && input.length() == 1;
+	}
+	
+	public static void setUpCaves()
+	{
+		//This is where you edit your caves!!!
+	}
+	
+	public void goToRoom(int direction)
+	{
+		if(borderingRooms[direction] != null && doors[direction] != null && doors[direction].isOpen())
+		{
+			CaveExplorer.currentRoom.leave();
+			CaveExplorer.currentRoom = borderingRooms[direction];
+			CaveExplorer.currentRoom.enter();
+			CaveExplorer.inventory.updateMap();
+		}
+		else
+		{
+			//print red text.
+			System.err.println("Youc can't do that!");
+		}
+	}
+
+	/**
+	 * oD(0) = 2
+	 * oD(1) = 3
+	 * @param dir
+	 * @return
+	 */
+	public static int oppositeDirection(int dir)
+	{
+		int arr[]= {2,3,0,1};
+		return arr[dir];
+	}
+	
 	public void setDirections() 
 	{
 		directions = "";
@@ -49,13 +125,10 @@ public class CaveRoom {
 			
 			if(!doorFound)
 			{
-				directions = "You are trap.";
+				directions = "There is no way out. You are trap!";
 			}
 		}	
-	
 	}
-	
-	
 	/**
 	 * converts an integer to a direction:
 	 * 	0->"the North"
@@ -65,46 +138,31 @@ public class CaveRoom {
 	public static String toDirection(int dir)
 	{
 		String direction[] = {"the North", "the East", "the South", "the West"}; 
-		
 		 return direction[dir];
 		
 	}
-
 	public void setDefaultContents(String defaultContents)
 	{
 		this.defaultContents = defaultContents;
 	}
-
 	public String getDescription() {
 		return description;
 	}
-
-
 	public void setDescription(String description) {
 		this.description = description;
 	}
-
-
 	public String getDirections() {
 		return directions;
 	}
-
-
 	public void setDirections(String directions) {
 		this.directions = directions;
 	}
-
-
 	public String getContents() {
 		return contents;
 	}
-
-
 	public void setContents(String contents) {
 		this.contents = contents;
 	}
-
-
 	public void setDefaultContent(String defaultContent) {
 		this.defaultContent = defaultContent;
 	}
