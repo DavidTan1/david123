4f9df4121c40469f22c51e061cb6c68d8da543a4
BT_1N3_17 <BT_1N3_17@BT_1N3_17.bktech.local>
2017-10-31 14:27:42 -0400

    fix bugs

--- src/caveExplorer/Inventory.java
+++ src/caveExplorer/Inventory.java
@@ -2,77 +2,49 @@ package caveExplorer;
 
 public class Inventory {
 	
-	
 	private String map;
 	
-	public Inventory()
-	{
+	public Inventory(){
 		updateMap();
 	}
 	public void updateMap() {
 		map = " ";
-		//make hor. line across top:
-		for(int i =0; i< CaveExplorer.caves[0].length-1; i++)
-		{
-			map+="____"; //four '_'
+		//make hor.line across top:
+		for(int i =0;i<CaveExplorer.caves[0].length-1;i++) {
+			map+="____";
 		}
-		
-		map+="___"; //three '_'
-		
-		for(CaveRoom[] row: CaveExplorer.caves)
-		{
-			for(int i =0; i<3; i++)
-			{
+		// for symmetry, add only three underscores;w
+		map += "___\n";
+		for(CaveRoom[] row: CaveExplorer.caves) {
+			for(int i =0;i<3;i++) {
 				String text = "";
-		
-				for(CaveRoom cr: row)
-				{
-					if(cr.getDoor(CaveRoom.WEST)!=null && cr.getDoor(CaveRoom.WEST).isOpen())
-					{
-						text += " ";
-					}
-					else
-					{
+				for(CaveRoom cr:row) {
+					//if a wall is presented draw a line otherwise draw a "\"
+					if(cr.getDoor(CaveRoom.WEST) != null && cr.getDoor(CaveRoom.WEST).isOpen()) {
+						text+= " ";
+					}else {
 						text += "|";
 					}
-					if(i==0)
-					{
+					if(i == 0) {
 						text += "  ";
-					}
-					else
-					{
-						if(i==1)
-						{
-							text += "  "+cr.getContents() + "  ";
-						}
-						else
-						{
-							if(i==2)
-							{
-								if(cr.getDoor(CaveRoom.SOUTH) != null && cr.getDoor(CaveRoom.SOUTH).isOpen())
-								{
-									text +="   ";
-								}
-								else
-								{
-									text += "____";
-								}
-							}
-							
-							
-							text += "|";
-							map  += text + "\n";
-						
+					}else if(i == 1) {
+						text += " " + cr.getContents();
+					}else if ( i == 2) {
+						if(cr.getDoor(CaveRoom.SOUTH) != null && cr.getDoor(CaveRoom.SOUTH).isOpen()){
+							text += "   "; 
+						}else {
+							text += "____";
 						}
 					}
-					
-				}
+				}//last cave room, in row
+				text += "|";
+				map += text + "\n";
 			}
-		}	
-		
+		}
 	}
 	
 	public String getDescription() {
-		return "There is nothing in your inventory.";
+		return map;
+		//return "There is nothing in your inventory.";
 	}
 }
